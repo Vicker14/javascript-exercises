0a10b04a8bbc363120a3eef280c9f8b6f801ee2c
Vicker <vcalero2001@gmail.com>
2025-04-25 15:37:07 +0200

    Exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argument) {
+    let finalArray = array;
+    for (const arg of argument) {
+        while (finalArray.includes(arg)) {
+            argumentIndex = array.indexOf(arg);
+            finalArray.splice(argumentIndex, 1);    
+        }
+    }
+    return finalArray;
 };
 
 // Do not edit below this line
