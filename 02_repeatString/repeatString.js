4714aa970e54b2970e9649f465b0487f9b12fd87
Vicker <vcalero2001@gmail.com>
2025-04-25 04:53:36 +0200

    Exercise 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    if (times < 0){
+        return "ERROR"
+    }
+    let result = '';
+    for (let index = 0; index < times; index++) {
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
