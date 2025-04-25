68b906e8f8e99971630fc1a5a4883961222c5828
Vicker <vcalero2001@gmail.com>
2025-04-25 16:01:20 +0200

    Exercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(intBegin, intEnd) {
+    let sum = 0;
 
+    if (intBegin < 0 || intEnd < 0)
+        return "ERROR";
+    if (!Number.isInteger(intBegin) || !Number.isInteger(intEnd))
+        return "ERROR";
+
+    if (intBegin <= intEnd) {
+        for (let int = intBegin; int <= intEnd; int++) {
+            sum += int;
+        }
+        return sum;
+    } else {
+        for (let int = intEnd; int <= intBegin; int++) {
+            sum += int;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
