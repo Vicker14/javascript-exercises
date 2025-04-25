25ba86061a91a72f79416a250f6d75874b28b538
Vicker <vcalero2001@gmail.com>
2025-04-25 16:26:28 +0200

    Exercise 07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemperature) {
+  const result = ((farenheitTemperature - 32) * 5 / 9);
+  return Number(
+    result.toFixed(
+      Number.isInteger(result) ? 0 : 1
+    )
+  );
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  const result = ((celsiusTemperature * 9 / 5) + 32);
+
+  return Number(
+    result.toFixed(
+      Number.isInteger(result) ? 0 : 1
+    )
+  );
 };
 
 // Do not edit below this line
