02832a6f8c3948c16d022abdcea35873dc8a8fd6
Vicker <vcalero2001@gmail.com>
2025-05-08 23:25:40 +0200

    Exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(peopleList) {
+    let oldestPerson = peopleList[0];
+    for (let i = 1; i < peopleList.length; i++) {
+        const currentPersonAge = getAge(peopleList[i].yearOfBirth, peopleList[i].yearOfDeath);
+        const oldestPersonAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        if (currentPersonAge > oldestPersonAge) {
+            oldestPerson = peopleList[i];
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
