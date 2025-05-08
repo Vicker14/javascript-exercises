c96b5a0480d36673774529fa0dbdde7bfcf36ce5
Vicker <vcalero2001@gmail.com>
2025-05-08 22:08:17 +0200

    Exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+	word = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+	let ini = 0;
+	let end = word.length - 1;
+	while (ini < word.length / 2) {
+		if (word[ini] != word[end]) return false;
+		ini++;
+		end--;
+	}
+	return true;
 };
 
 // Do not edit below this line
