4dc4637caf0e8e25097aa99371db5db7675c7895
Vicker <vcalero2001@gmail.com>
2025-05-08 22:49:02 +0200

    Exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    if (pos == 1 || pos == 2) return 1;
+    if (pos == 0) return 0;
+    if (pos < 0) return "OOPS";
+
+    let previous = 1;
+    let result = 1;
+    
+    for (let i = 2; i < pos; i++) {
+        let aux = result;
+        result += previous;
+        previous = aux;
+    }
+    return result;
 
 };
 
